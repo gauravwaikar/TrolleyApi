ae438710ab82acd1916c82f8b538e295cfe747c6
Gaurav Waikar <Gaurav_Waikar@amp.com.au>
2021-07-07 14:55:55 +1000

    Sort and TrolleyTotal Controller

---
+++ TrolleyApi/Controllers/SortController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrolleyApi.Exercise2.Services;
+
+namespace TrolleyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SortController : ControllerBase
+    {
+        private readonly ISortService _sortService;
+
+        public SortController(ISortService sortService)
+        {
+            _sortService = sortService;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get(
+            [FromQuery]
+            [Required]
+            [SortOptionValidator]
+        string sortOption)
+            => Ok(await _sortService.Sort(sortOption.ToUpperInvariant()));
+    }
+}
---
+++ TrolleyApi/Controllers/TrolleyTotalController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TrolleyApi.Exercise3.Domain;
+using TrolleyApi.Exercise3.Services;
+
+namespace TrolleyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrolleyTotalController : ControllerBase
+    {
+        
+        private readonly ILogger<TrolleyTotalController> _logger;
+        private readonly ITrolleyTotalService _trolleyTotalService;
+
+        public TrolleyTotalController(ITrolleyTotalService trolleyTotalService,
+            ILogger<TrolleyTotalController> logger)
+        {
+            
+            _logger = logger;
+            _trolleyTotalService = trolleyTotalService;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CalculateTrolleyTotalRequest request)
+        {
+            
+            _logger.LogInformation("Inside TrolleyTotal controller");
+            _logger.LogInformation(System.Text.Json.JsonSerializer.Serialize(request));
+            return Ok(_trolleyTotalService.Calculate(request));
+        }
+    }
+}
